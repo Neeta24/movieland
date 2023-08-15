e5e99bda361a45cb2381b59625aaff9e1457e6c7
Neeta24 <neeta.sinha.24@gmail.com>
2023-08-15 21:09:50 +0600

    mapping the movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
 import {useEffect, useState} from 'react'
 import './App.css';
-import SearchIcon from './search.svg'
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard'
+
 
 //d723dd67
 
 const Api_Url = 'http://www.omdbapi.com?apikey=d723dd67';
 
-const movie1 =
-  {
-    "Title": "Italian Spiderman",
-    "Year": "2007",
-    "imdbID": "tt2705436",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg"
-}
+
 
 const App =()=> {
   
-  const [movies,setMovies] = useState([])
+  const [movies,setMovies] = useState([]);
+
+  const[searchTerm,setSearchTerm] = useState('');
   const searchMovies = async(title)=>{
 
     const response = await fetch(`${Api_Url}&s=${title}`)
     const data = await response.json();
-    console.log(data.Search);
+    setMovies(data.Search);
+    console.log(data.Search)
   }
   useEffect(()=>{
    searchMovies('Spiderman');
@@ -32,23 +30,29 @@ const App =()=> {
    <div className='app'>
     <h1>MovieLand</h1>
     <div className='search'>
-      <input placeholder='search for movies' value="Superman" onChange={()=>{}} />
-      <img src={SearchIcon} alt="search" onClick={()=>{}}/>
+      <input placeholder='search for movies' value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)} />
+      <img src={SearchIcon} alt="search" onClick={()=>searchMovies(searchTerm)}/>
     </div>
 
-    <div className='container'>
-   <div className='movie'>
-    <div>
-      <p>{movie1.Year}</p>
-    </div>
 
-    <div>
-      <img src={movie1.Poster !== 'N/A'? movie1.Poster:'https//via.placeholder.com/400'} alt={movie1.Title} />
-    </div>
-   </div>
-    </div>
+{
+  movies?.length>0 
+  ? ( <div className='container'>
+  {movies.map((movie)=>(<MovieCard key ={movie.imdbID} movie ={movie}></MovieCard>))}
+  </div>
+  ):
+  (
+   <div className='empty'>
+    <h2>No movies found</h2>
    </div>
   )
+
+
+
+}
+   
+   </div>
+  );
 }
 
 export default App
---
+++ src/MovieCard.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+// eslint-disable-next-line react/prop-types
+const MovieCard = ({movie}) => {
+  return (
+   
+ <div className='movie'>
+ <div>
+   <p>{movie.Year}</p>
+ </div>
+
+ <div>
+   <img src={movie.Poster !== 'N/A'? movie.Poster:'https//via.placeholder.com/400'} alt={movie.Title} />
+ </div>
+ <div>
+   <span>{movie.Type}</span>
+   <h3>{movie.Title}</h3>
+ </div>
+</div>
+)
+ 
+};
+
+export default MovieCard;
+
